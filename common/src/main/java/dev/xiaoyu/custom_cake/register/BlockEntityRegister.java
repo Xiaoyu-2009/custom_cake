211bcfd504c4b9163bf477b5a4b0571b3f884f10
Xiaoyu_2009 <3551371733@qq.com>
2026-03-13 04:41:33 +0800

    更改了些许东西11

--- common/src/main/java/dev/xiaoyu/custom_cake/register/BlockEntityRegister.java
+++ common/src/main/java/dev/xiaoyu/custom_cake/register/BlockEntityRegister.java
@@ -11,8 +11,7 @@ public final class BlockEntityRegister {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(CustomCake.MOD_ID, Registries.BLOCK_ENTITY_TYPE);
 
     public static final RegistrySupplier<BlockEntityType<BaseCakeBlockEntity>> BASE_CAKE_BLOCK_ENTITY = BLOCK_ENTITIES.register(
-        "base_cake_block_entity",
-        () -> BlockEntityType.Builder.of(BaseCakeBlockEntity::new, Blocks.CAKE).build(null)
+        "base_cake_block_entity", () -> BlockEntityType.Builder.of(BaseCakeBlockEntity::new, Blocks.CAKE).build(null)
     );
 
     public static void init() {
